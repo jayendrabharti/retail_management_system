417958f833fa3c0cddc5746c841060366e46c845
{Jayendra <jay.bharti2804@gmail.com>
2025-07-04 12:50:36 +0530

    pre sprint checkpoint

--- components/home/DashboardShowcase.tsx
+++ components/home/DashboardShowcase.tsx
@@ -6,17 +6,18 @@ export default function DashboardShowcase() {
   const { theme } = useTheme();
 
   return (
-    <Reveal className="p-5 md:p-10 lg:p-20 overflow-hidden">
+    <Reveal className="p-5 h-max md:p-10 lg:p-20">
       <Image
+        suppressHydrationWarning
         src={
           theme === "dark"
             ? "/images/dashboard-showcase-dark.png"
             : "/images/dashboard-showcase-light.png"
         }
         alt="Tilted"
-        width={600}
-        height={600}
-        className="w-full h-full rounded-lg border-4 border-muted"
+        width={1000}
+        height={1000}
+        className="w-full h-max rounded-lg border-4 border-muted"
       />
     </Reveal>
   );
